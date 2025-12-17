cc37960e99cd6fb6ff0c3fa793c633678430ec06
Nutria <qkrwldn2016@gmail.com>
2025-12-17 22:15:32 +0900

    chore: rename MztkBeApplication.java package, reconstruct directory and add .gitkeep files

--- src/main/java/momzzangseven/mztkbe/MztkBeApplication.java
+++ src/main/java/momzzangseven/mztkbe/MztkBeApplication.java
@@ -1,4 +1,4 @@
-package momzzangseven.mztkbe.bootstrap;
+package momzzangseven.mztkbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
