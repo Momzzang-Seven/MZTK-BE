5e4fd82d22724e50fd4678ce2786d268bc0c5b30
RaewooKang <raewoo0908@gmail.com>
2025-12-30 15:16:01 +0900

    [MOM-168] feat: domain layer implementation. necessary error code implementation

---
+++ src/main/java/momzzangseven/mztkbe/global/error/user/IllegalAdminGrantException.java
@@ -0,0 +1,11 @@
+package momzzangseven.mztkbe.global.error.user;
+
+import momzzangseven.mztkbe.global.error.BusinessException;
+import momzzangseven.mztkbe.global.error.ErrorCode;
+
+public class IllegalAdminGrantException extends BusinessException {
+
+    public IllegalAdminGrantException() {
+        super(ErrorCode.ILLEGAL_ADMIN_GRANT, ErrorCode.ILLEGAL_ADMIN_GRANT.getMessage());
+    }
+}
