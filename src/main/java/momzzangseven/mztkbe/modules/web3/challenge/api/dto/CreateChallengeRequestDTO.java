8e56cd47c3d0ed4e7004d83bb299f6a25f7c8681
RaewooKang <raewoo0908@gmail.com>
2026-01-20 17:11:52 +0900

    [MOM-170] feat: api layer complete

---
+++ src/main/java/momzzangseven/mztkbe/modules/web3/challenge/api/dto/CreateChallengeRequestDTO.java
@@ -0,0 +1,8 @@
+package momzzangseven.mztkbe.modules.web3.challenge.api.dto;
+
+import jakarta.validation.constraints.NotNull;
+import momzzangseven.mztkbe.modules.web3.challenge.domain.model.ChallengePurpose;
+
+public record CreateChallengeRequestDTO(
+    @NotNull(message = "Purpose must not be null") ChallengePurpose purpose,
+    @NotNull(message = "Wallete address must not be null") String walletAddress) {}
