7da69210db199d97d64b480b8714e40a995af8f6
RaewooKang <raewoo0908@gmail.com>
2026-01-27 21:47:20 +0900

    [MOM-169] refactor: refactor domain layer

--- src/main/java/momzzangseven/mztkbe/modules/web3/wallet/domain/model/WalletStatus.java
+++ src/main/java/momzzangseven/mztkbe/modules/web3/wallet/domain/model/WalletStatus.java
@@ -1,13 +1,28 @@
 package momzzangseven.mztkbe.modules.web3.wallet.domain.model;
 
-/** Wallet status enum */
+/** Wallet status enum
+ *<p>Status Flow:
+ *  <ul>
+ *    <li>ACTIVE → UNLINKED (User unlink his wallet by himself)</li>
+ *    <li>ACTIVE → USER_DELETED (User withdraw)</li>
+ *    <li>ACTIVE → BLOCKED (Blocked by admin)</li>
+ *    <li>UNLINKED → HARD_DELETED (Hard deleted by wallet hard delete scheduler)</li>
+ *    <li>USER_DELETED → HARD_DELETED (Hard deleted by user hard delete scheduler)</li>
+ *  </ul>
+ */
 public enum WalletStatus {
   /** Active and usable */
   ACTIVE,
 
-  /** Deactivated by user (soft delete) */
-  INACTIVE,
+  /** Unlinked by user himself */
+  UNLINKED,
+
+  /** User deleted */
+  USER_DELETED,
 
   /** Blacklisted by admin */
-  BLACKLISTED
+  BLOCKED,
+
+  /** Hard deleted by scheduler */
+  HARD_DELETED
 }
