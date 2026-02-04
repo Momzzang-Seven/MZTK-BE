60f59fad895be8da56f5089d59af06be0da20bb2
RaewooKang <raewoo0908@gmail.com>
2026-02-04 15:14:32 +0900

    [MOM-206] feat: infrastructure layer implementation. Except for Kakao Geocoding API connection

---
+++ src/main/java/momzzangseven/mztkbe/modules/location/infrastructure/pesistence/adapter/LocationPersistenceAdapter.java
@@ -0,0 +1,42 @@
+package momzzangseven.mztkbe.modules.location.infrastructure.pesistence.adapter;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import momzzangseven.mztkbe.modules.location.application.port.out.LoadLocationPort;
+import momzzangseven.mztkbe.modules.location.application.port.out.SaveLocationPort;
+import momzzangseven.mztkbe.modules.location.domain.model.Location;
+import momzzangseven.mztkbe.modules.location.infrastructure.pesistence.entity.LocationEntity;
+import momzzangseven.mztkbe.modules.location.infrastructure.repository.LocationJpaRepository;
+import org.springframework.stereotype.Component;
+
+/** Location persistence adapter - SaveLocationPort, LoadLocationPort implementation */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LocationPersistenceAdapter implements LoadLocationPort, SaveLocationPort {
+
+  private final LocationJpaRepository locationJpaRepository;
+
+  // SaveLocationPort 구현
+  @Override
+  public Location save(Location location) {
+    LocationEntity entity = LocationEntity.from(location);
+    LocationEntity savedEntity = locationJpaRepository.save(entity);
+    return savedEntity.toDomain();
+  }
+
+  // LoadLocationPort 구현
+  @Override
+  public Optional<Location> findByLocationId(Long id) {
+    return locationJpaRepository.findById(id).map(LocationEntity::toDomain);
+  }
+
+  @Override
+  public List<Location> findByUserId(Long userId) {
+    return locationJpaRepository.findByUserIdOrderByRegisteredAtDesc(userId).stream()
+        .map(LocationEntity::toDomain)
+        .toList();
+  }
+}
---
+++ src/main/java/momzzangseven/mztkbe/modules/location/infrastructure/repository/LocationJpaRepository.java
@@ -0,0 +1,13 @@
+package momzzangseven.mztkbe.modules.location.infrastructure.repository;
+
+import java.util.List;
+import momzzangseven.mztkbe.modules.location.infrastructure.pesistence.entity.LocationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/** Location JPA Repository */
+@Repository
+public interface LocationJpaRepository extends JpaRepository<LocationEntity, Long> {
+  /** Get List of Location by userId in order registered_at desc */
+  List<LocationEntity> findByUserIdOrderByRegisteredAtDesc(Long userId);
+}
