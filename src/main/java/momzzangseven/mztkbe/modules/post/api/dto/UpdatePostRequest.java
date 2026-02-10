4d585f13aaf6346c79ade06173e894037757b208
wdong218 <wdh8890@naver.com>
2026-02-11 00:04:33 +0900

    [MOM-101] 리뷰 반영

--- src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
+++ src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
@@ -1,9 +1,5 @@
 package momzzangseven.mztkbe.modules.post.api.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
-public record UpdatePostRequest(
-    @NotBlank(message = "제목을 입력해주세요.") String title,
-    @NotBlank(message = "내용을 입력해주세요.") String content,
-    List<String> imageUrls) {}
+public record UpdatePostRequest(String title, String content, List<String> imageUrls) {}
