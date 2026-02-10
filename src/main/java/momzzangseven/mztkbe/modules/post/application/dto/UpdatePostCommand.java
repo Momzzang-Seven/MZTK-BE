8ebd25ce433d1531d64689a1aa45c810cbcecb0d
wdong218 <wdh8890@naver.com>
2026-02-10 22:57:47 +0900

    [MOM-101] Test code&Bug fix

--- src/main/java/momzzangseven/mztkbe/modules/post/application/dto/UpdatePostCommand.java
+++ src/main/java/momzzangseven/mztkbe/modules/post/application/dto/UpdatePostCommand.java
@@ -6,11 +6,16 @@ import momzzangseven.mztkbe.global.error.post.PostInvalidInputException;
 public record UpdatePostCommand(String title, String content, List<String> imageUrls) {
 
   public void validate() {
-    if (title == null || title.isBlank()) {
-      throw new PostInvalidInputException("수정할 제목은 필수입니다.");
+    if (title != null && title.isBlank()) {
+      throw new PostInvalidInputException("수정할 제목은 비워둘 수 없습니다.");
     }
-    if (content == null || content.isBlank()) {
-      throw new PostInvalidInputException("수정할 내용은 필수입니다.");
+
+    if (content != null && content.isBlank()) {
+      throw new PostInvalidInputException("수정할 내용은 비워둘 수 없습니다.");
+    }
+
+    if (title == null && content == null && imageUrls == null) {
+      throw new PostInvalidInputException("수정할 값이 없습니다.");
     }
   }
 
