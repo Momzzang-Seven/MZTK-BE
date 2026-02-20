73b1f336c6a893d2d6a73085035b01610ace2f3b
Nutria <qkrwldn2016@gmail.com>
2026-02-20 20:17:55 +0900

    [MOM-223] refactor: update spotless configuration and clean up generated source handling
    Signed-off-by: Nutria <qkrwldn2016@gmail.com>

--- src/main/resources/db/migration/V013__narrow_web3_reference_uniqueness.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS uk_web3_tx_reference;
-
-CREATE UNIQUE INDEX IF NOT EXISTS uk_web3_tx_reference_level_up
-    ON web3_transactions(reference_type, reference_id)
-    WHERE reference_type = 'LEVEL_UP_REWARD';
