8c989cc5d284c09b4db74f05cedb4e4a70a71f7d
RaewooKang <raewoo0908@gmail.com>
2026-03-11 15:01:37 +0900

    [MOM-279] feat: add error code, make corresponding error class, and leverage it in command.

--- src/main/java/momzzangseven/mztkbe/global/error/image/InvalidImageExtensionException.java
+++ src/main/java/momzzangseven/mztkbe/global/error/image/InvalidImageExtensionException.java
@@ -4,7 +4,7 @@ import momzzangseven.mztkbe.global.error.BusinessException;
 import momzzangseven.mztkbe.global.error.ErrorCode;
 
 public class InvalidImageExtensionException extends BusinessException {
-    public InvalidImageExtensionException(String msg) {
-        super(ErrorCode.IMAGE_INVALID_EXTENSION, msg);
-    }
+  public InvalidImageExtensionException(String msg) {
+    super(ErrorCode.IMAGE_INVALID_EXTENSION, msg);
+  }
 }
