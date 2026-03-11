0fc28f73669192ad668f44304d1eeec72f637b11
Nutria <qkrwldn2016@gmail.com>
2026-03-11 23:28:32 +0900

    [MOM-115] refactor: remove exist code to match with images table.
    Signed-off-by: Nutria <qkrwldn2016@gmail.com>

--- src/main/java/momzzangseven/mztkbe/modules/verification/application/port/in/GetVerificationStatusUseCase.java
+++
@@ -1,9 +0,0 @@
-package momzzangseven.mztkbe.modules.verification.application.port.in;
-
-import java.util.Optional;
-import momzzangseven.mztkbe.modules.verification.domain.model.VerificationRequest;
-
-/** Query use case for loading a single verification by public identifier. */
-public interface GetVerificationStatusUseCase {
-  Optional<VerificationRequest> execute(Long userId, String verificationId);
-}
