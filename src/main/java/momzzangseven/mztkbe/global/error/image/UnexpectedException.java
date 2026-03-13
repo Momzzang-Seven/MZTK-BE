9d8876e4c9c60d430ad7452d97f339fdd7c4bf1e
RaewooKang <raewoo0908@gmail.com>
2026-03-13 14:51:03 +0900

    [MOM-285] refactor: detach domain with S3 folder structure.

---
+++ src/main/java/momzzangseven/mztkbe/global/error/image/UnexpectedException.java
@@ -0,0 +1,7 @@
+package momzzangseven.mztkbe.global.error.image;
+
+public class UnexpectedException extends RuntimeException {
+  public UnexpectedException(String message) {
+    super(message);
+  }
+}
