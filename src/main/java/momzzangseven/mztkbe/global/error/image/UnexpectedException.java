9d43e4d86f3c499f64e394453729ecf7620cfc03
RaewooKang <raewoo0908@gmail.com>
2026-03-13 15:19:14 +0900

    [MOM-285] refactor: rename exception class

--- src/main/java/momzzangseven/mztkbe/global/error/image/UnexpectedException.java
+++
@@ -1,7 +0,0 @@
-package momzzangseven.mztkbe.global.error.image;
-
-public class UnexpectedException extends RuntimeException {
-  public UnexpectedException(String message) {
-    super(message);
-  }
-}
