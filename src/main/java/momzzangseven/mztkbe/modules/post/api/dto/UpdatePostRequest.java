d192d73bd8152081f2176278f18530058ef17e20
RaewooKang <raewoo0908@gmail.com>
2026-03-24 14:17:21 +0900

    [MOM-298] refactor: add validation rules for each element of imageIds

--- src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
+++ src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
@@ -1,6 +1,8 @@
 package momzzangseven.mztkbe.modules.post.api.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 public record UpdatePostRequest(
-    String title, String content, List<Long> imageIds, List<String> tags) {}
+    String title, String content, List<@NotNull @Positive Long> imageIds, List<String> tags) {}
