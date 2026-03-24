5fa1437437bc490327f9f6d4812e9251b071bc84
Nutria <qkrwldn2016@gmail.com>
2026-03-24 20:20:38 +0900

    refactor: remove answer_images table and update related DTOs and services to use image IDs

--- src/main/java/momzzangseven/mztkbe/modules/image/application/port/in/GetImagesByReferencesUseCase.java
+++
@@ -1,9 +0,0 @@
-package momzzangseven.mztkbe.modules.image.application.port.in;
-
-import momzzangseven.mztkbe.modules.image.application.dto.GetImagesByReferencesCommand;
-import momzzangseven.mztkbe.modules.image.application.dto.GetImagesByReferencesResult;
-
-public interface GetImagesByReferencesUseCase {
-
-  GetImagesByReferencesResult execute(GetImagesByReferencesCommand command);
-}
