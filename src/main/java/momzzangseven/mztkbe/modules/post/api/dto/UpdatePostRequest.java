0d7bc504d02b1db535ad4aa749ef85773358cd89
RaewooKang <raewoo0908@gmail.com>
2026-03-25 10:29:43 +0900

    [MOM-298] chore: spotlessApply

--- src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
+++ src/main/java/momzzangseven/mztkbe/modules/post/api/dto/UpdatePostRequest.java
@@ -1,6 +1,5 @@
 package momzzangseven.mztkbe.modules.post.api.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
