2a3f01845dd2ff876373d19b448c9ff15a8b3e9a
RaewooKang <raewoo0908@gmail.com>
2026-05-06 18:08:34 +0900

    [MOM-391] remove: legacy cipher / port / KEK env 일괄 삭제
    삭제 파일 (4):
    - web3/treasury/infrastructure/adapter/TreasuryKeyCipher.java (AES-GCM helper)
    - web3/treasury/infrastructure/adapter/TreasuryKeyEncryptionAdapter.java
    - web3/treasury/application/port/out/TreasuryKeyEncryptionPort.java
    - web3/treasury/application/port/out/SaveTreasuryKeyPort.java (impl 은 4-1 에서 이미 제거)
    
    수정 파일:
    - application.yml: 3 KEK 라인 제거
      * web3.reward-token.treasury.key-encryption-key-b64
      * web3.eip7702.sponsor.key-encryption-key-b64
      * web3.execution.internal.signer.key-encryption-key-b64
    - RewardTreasurySignerProperties: keyEncryptionKeyB64 필드 + getter + binder 제거
    - SponsorTreasurySignerProperties: keyEncryptionKeyB64 필드 + getter + key-encryption-key-b64 일치성 검증 로직 제거
    
    테스트 정리:
    - TreasuryKeyCipherTest / TreasuryKeyEncryptionAdapterTest: 폐기
    - SponsorTreasurySignerPropertiesTest: getKeyEncryptionKeyB64() assertion + KEK 일치성 검증 케이스 제거

--- src/main/java/momzzangseven/mztkbe/modules/web3/treasury/application/port/out/TreasuryKeyEncryptionPort.java
+++
@@ -1,8 +0,0 @@
-package momzzangseven.mztkbe.modules.web3.treasury.application.port.out;
-
-public interface TreasuryKeyEncryptionPort {
-
-  String encrypt(String plaintext, String keyB64);
-
-  String generateKeyB64();
-}
